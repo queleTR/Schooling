4911da54909e917767fac1c61f3e989a0b3a7c64
queletr <queleyt2005@gmail.com>
2024-09-20 10:21:21 +0200

    init

---
+++ src/main/java/de/muhammed/graphql_kurs/GraphqlKursApplication.java
@@ -0,0 +1,13 @@
+package de.muhammed.graphql_kurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlKursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlKursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/muhammed/graphql_kurs/GraphqlKursApplicationTests.java
@@ -0,0 +1,13 @@
+package de.muhammed.graphql_kurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlKursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
